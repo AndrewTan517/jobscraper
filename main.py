4f8d416886a965351dfd084d561e2c4897387f08
AndrewTan517 <164039150+AndrewTan517@users.noreply.github.com>
2026-06-05 17:43:43 +0800

    feat: initial web scraper (#2)
    * chore: add requests and beautifulsoup4 dependencies
    
    * feat: create main.py scraper entrypoint script
    
    * feat: create jobscraper package structure

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import argparse
+
+SCRAPERS = {}
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="site")
+
+    for site, (add_arguments, scrape) in SCRAPERS.items():
+        subparser = subparsers.add_parser(site)
+        add_arguments(subparser)
+        subparser.set_defaults(scrape=scrape)
+
+    return parser
+
+
 def main():
-    print("Hello from jobscraper!")
+    parser = create_parser()
+    args = parser.parse_args()
+    args.scrape(args)
 
 
 if __name__ == "__main__":
